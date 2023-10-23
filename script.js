129d253074baf87e966a4ce5dcfbbe204bf5f5a0
Marlesli <122844039+Marlesli@users.noreply.github.com>
2023-10-23 06:20:18 +0000

    pw validate

---
+++ script.js
@@ -0,0 +1,12 @@
+function matchPassword() {  
+  var pw1 = document.getElementById("pswd1");  
+  var pw2 = document.getElementById("pswd2");  
+  if(pswd1 != pswd2)  
+  {   
+    alert("Passwords did not match");  
+  } else 
+  
+  {  
+    alert("Password created successfully");  
+  }  
+}  
