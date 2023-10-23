68305f9ec3a458a98eafd69bf577f7eb2f1dc9cd
Marlesli <122844039+Marlesli@users.noreply.github.com>
2023-10-23 06:58:41 +0000

    username validation

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ function matchPassword() {
     alert("Password created successfully");  
   }  
 }  
+
+// Array of existing valid usernames
+const existingUsernames = ["Admin", "Harry", "Elizabeth", "Shana"];
+
+function validateUsername() {
+  // Get the username entered by the user
+  const username = document.getElementById('username').value;
+
+  // Check if the entered username is in the array of existing usernames
+  if (existingUsernames.includes(username)) {
+    // Username is already taken, display an error message
+    alert('This username is already taken. Please choose a different one.');
+  } else {
+    // Username is not taken, you can proceed or display a success message
+    alert('Username is available. You can use it.');
+  }
+}
