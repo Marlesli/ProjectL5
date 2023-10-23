93856142fa56884ff7cb0ad93e54a2b8928469af
Marlesli <122844039+Marlesli@users.noreply.github.com>
2023-10-23 21:22:23 +0000

    adjust JS validation

--- script.js
+++ script.js
@@ -1,29 +1,14 @@
-function matchPassword() {  
-  var pw1 = document.getElementById("pswd1");  
-  var pw2 = document.getElementById("pswd2");  
-  if(pswd1 != pswd2)  
-  {   
-    alert("Passwords did not match");  
-  } else 
-  
-  {  
-    alert("Password created successfully");  
-  }  
-}  
-
-// Array of existing valid usernames
-const existingUsernames = ["Admin", "Harry", "Elizabeth", "Shana"];
-
-function validateUsername() {
-  // Get the username entered by the user
-  const username = document.getElementById('username').value;
-
-  // Check if the entered username is in the array of existing usernames
-  if (existingUsernames.includes(username)) {
-    // Username is already taken, display an error message
-    alert('This username is already taken. Please choose a different one.');
-  } else {
-    // Username is not taken, you can proceed or display a success message
-    alert('Username is available. You can use it.');
+function checkPassword(){
+  let password = document.getElementById("password").value;
+  let confirmPassword = document.getElementById("confirmPassword").value;
+  console.log(password,confirmPassword);
+  let message = document.getElementById("message");
+  if(password.length != 0){
+    if(password == confirmPassword){
+      message.textcontent = "Passwords Match";
+    
+    }
   }
 }
+
+
