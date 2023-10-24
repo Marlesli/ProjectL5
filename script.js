e44b9c27bfc92815deff250abc4e7856c4a8f360
Marlesli <122844039+Marlesli@users.noreply.github.com>
2023-10-24 02:22:21 +0000

    modify password validation

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-function checkPassword(){
-  let password = document.getElementById("password").value;
-  let confirmPassword = document.getElementById("confirmPassword").value;
-  console.log(password,confirmPassword);
-  let message = document.getElementById("message");
-  if(password.length != 0){
-    if(password == confirmPassword){
-      message.textcontent = "Passwords Match";
-    
-    }
-  }
+function checkPassword(Form) { 
+    password1 = Form.password1.value; 
+    password2 = Form.password2.value; 
+
+    // If password not entered 
+    if (password1 == '') 
+        alert ("Please enter Password"); 
+
+    // If confirm password not entered 
+    else if (password2 == '') 
+        alert ("Please enter confirm password"); 
+
+    // return False if unmatched.     
+    else if (password1 != password2) { 
+        alert ("Passwords did not match: Please try again...") 
+        return false; 
+    } 
+
+    // If same return True if match. 
+    else{ 
+        alert("Passwords Match: You may proceed") 
+        return true; 
+    } 
+} 
+
+
+// Array of existing valid usernames
+const existingUsernames = ["Admin", "Harry", "Elizabeth", "Shana"];
+
+function isUsernameAvailable(username) {
+  return !existingUsernames.includes(username);
 }
-
-
